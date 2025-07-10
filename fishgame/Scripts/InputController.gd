464e83260baf841cf6f03b9a55557fc70e17ede7
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-07-10 11:54:59 -0400

    feat: Added coyote time

--- fishgame/Scripts/InputController.gd
+++ fishgame/Scripts/InputController.gd
@@ -6,14 +6,22 @@ const JUMP_VELOCITY = -325.0
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var jump_sfx: AudioStreamPlayer2D = $JumpSFX
+@onready var cyote_timer: Timer = $CyoteTimer
+
+var hasCyoteTime = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
+	if !is_on_floor():
 		velocity += get_gravity() * delta
+		
+		#Start cyote time
+		if !hasCyoteTime:
+			hasCyoteTime = true
+			cyote_timer.start()
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor() || Input.is_action_just_pressed("jump") && hasCyoteTime:
 		velocity.y = JUMP_VELOCITY
 		jump_sfx.play()
 
@@ -42,3 +50,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+#End of cyote time
+func _on_cyote_timer_timeout() -> void:
+	hasCyoteTime = false
