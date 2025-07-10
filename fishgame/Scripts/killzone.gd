a3ea15d9337ff201893a8a36758674a8fa8dc076
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-07-10 11:18:33 -0400

    feat: Added enemy

--- fishgame/Scripts/killzone.gd
+++ fishgame/Scripts/killzone.gd
@@ -4,8 +4,17 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You Died!")
+	
+	#Slow time
+	Engine.time_scale = 0.5
+	
+	#Find and remove players collider
+	body.get_node("CollisionShape2D").queue_free()
+	
+	#Start reset timer
 	timer.start()
 
 #Reset Scene
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
